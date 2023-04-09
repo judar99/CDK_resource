fee11a49cba1ede121d278db033e322a6703d5d5
judar99 <judaar.99@gmail.com>
2023-04-09 16:55:35 -0500

    update create and front

--- backend/create.py
+++ backend/create.py
@@ -1,64 +1,76 @@
 import json
 import boto3
-import json
-from pprint import pprint
 import uuid
+import cgi
+import base64
+from io import BytesIO
 
-dynamodb = boto3.client('dynamodb')
 
-
-def create_item(tablename, id_product, product, images,amount):
-    response = dynamodb.put_item(
-        TableName=tablename,
-        Item={
-            'id': {'S': id_product},
-            'product': {'S': product},
-            'amount' : {'S': amount},
-            'images': {'S': images}
-        }
-    )
-    return response
+s3_client = boto3.client('s3')
 
 def lambdaFuncion(event, context):
     
     try:
-    
+        
         body = json.loads(event['body'])
-        product_id = str(uuid.uuid4())
-        product = body['product']
-        images = body['images']
         amount = body['amount']
-    
-        table_name = 'CDK-InventoryTableFD135387-1PJCZGC6IAOMO'
-        create_item(table_name, product_id, product, images,amount)
-    
+        product  = body ['product']
+        base64_image = body['base64Image']
+        
+        image = BytesIO(base64.b64decode(base64_image))
+
+       
+        file_name = str(uuid.uuid4()) + '.png'
+      
+        image_url = 'https://cdk-imgbucketa8fcdd83-104wrly1ypfyx.s3.amazonaws.com/' + file_name
+        
+        s3_client.put_object(
+            Body=image,
+            Bucket='cdk-imgbucketa8fcdd83-104wrly1ypfyx',
+            Key=file_name,
+            ACL='public-read',
+            ContentType='image/png'
+        )
+        
+        # Agregar el registro de imagen en DynamoDB
+        dynamodb = boto3.client('dynamodb')
+        item_id = str(uuid.uuid4())
+        image_url = 'https://cdk-imgbucketa8fcdd83-104wrly1ypfyx.s3.amazonaws.com/' + file_name
+        
+        dynamodb.put_item(
+            TableName='CDK-InventoryTableFD135387-1PJCZGC6IAOMO',
+            Item={
+                'id': {'S': item_id},
+                'product': {'S': product},
+                'amount': {'S': amount},
+                'images': {'S': image_url}
+            }
+        )
+      
         return {
             'statusCode': 200,
             'headers': {
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE ,OPTIONS',
                 'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,X-Api-Key,X-Amz-Security-Token'
-    
             },
             'body': json.dumps({
-                
-                "messege":"Item created successfully"
+                'message': 'File uploaded successfully',
+                'url': image_url
                 
             })
         }
-    except Exception as e :
-        
+
+    except Exception as e:
         return {
             'statusCode': 400,
             'headers': {
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE ,OPTIONS',
                 'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,X-Api-Key,X-Amz-Security-Token'
-    
             },
             'body': json.dumps({
-                
-                "messege":str(e)
+                'message': 'Error uploading file: ' + str(e),
                 
             })
         }
